fdfbf473ee1237c213c8acecf0f9047bfb5e29a3
Esteban Secaida <esecaidavargas@gmail.com>
2024-09-17 16:38:30 -0600

    Added missing files 2

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import cx_Oracle
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+dsn_tns = cx_Oracle.makedsn("127.0.0.1", "1521", sid="Proyecto1BD")
+connection = cx_Oracle.connect(user="AP", password="donotgiveaway", dsn=dsn_tns)
+cursor = connection.cursor()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Check the connection
+print("Successfully connected to Oracle Database!")
+cursor.close()
+connection.close()
