5f5c9490f8b5512598943e72c76501e45781b0ae
liangbinjie <mailandresliang@gmail.com>
2024-10-16 18:59:48 -0600

    Update Frontend

---
+++ frontend/src/application/Controller/User/CountryController.java
@@ -0,0 +1,104 @@
+package application.Controller.User;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import application.ConnectDB;
+import application.Model.CountryRanking;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CountryController implements Initializable {
+	public int flag_height = 15;
+
+    @FXML
+    private TableColumn<CountryRanking, String> name;
+
+    @FXML
+    private TableColumn<CountryRanking, ImageView> flag;
+
+    @FXML
+    private TableColumn<CountryRanking, Integer> bronze;
+
+    @FXML
+    private TableColumn<CountryRanking, Integer> silver;
+
+    @FXML
+    private TableColumn<CountryRanking, Integer> gold;
+
+    @FXML
+    private TableColumn<CountryRanking, Integer> total;
+
+    @FXML
+    private TableView<CountryRanking> table;
+    
+    @FXML
+    private Button filterBtn;
+    
+    @FXML
+    private ComboBox olympicCB;
+    
+    @FXML
+    private ComboBox countryCB;
+    
+
+    // Sample Data for Countries
+    ObservableList<CountryRanking> countryList = FXCollections.observableArrayList(
+    		new CountryRanking("China", createFlagImageView("/resources/china.png"), 3, 5, 7, 15),
+            new CountryRanking("Costa Rica", createFlagImageView("/resources/crc.png"), 10, 8, 12, 30)
+    );
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	System.out.println();
+        // Set up the columns with the appropriate data properties
+        name.setCellValueFactory(new PropertyValueFactory<CountryRanking, String>("name"));
+        flag.setCellValueFactory(new PropertyValueFactory<CountryRanking, ImageView>("flag"));
+        bronze.setCellValueFactory(new PropertyValueFactory<CountryRanking, Integer>("bronze"));
+        silver.setCellValueFactory(new PropertyValueFactory<CountryRanking, Integer>("silver"));
+        gold.setCellValueFactory(new PropertyValueFactory<CountryRanking, Integer>("gold"));
+        total.setCellValueFactory(new PropertyValueFactory<CountryRanking, Integer>("total"));
+
+        // Load the sample data into the TableView
+        table.setItems(countryList);
+    }
+    
+    // Method to create ImageView with a fixed size for the flags
+    private ImageView createFlagImageView(String imagePath) {
+        ImageView imageView = new ImageView(new Image(getClass().getResource(imagePath).toExternalForm()));
+        imageView.setFitHeight(flag_height); // Set the desired height for the flag
+        imageView.setPreserveRatio(true); // Preserve the aspect ratio
+        return imageView;
+    }
+    
+    private void loadList(ResultSet set) throws SQLException {
+    	while (set.next()) {
+    		String countryName = set.getString("country_name");
+    		String imagePath = set.getString("image");
+    		int bronze = set.getInt("bronze");
+    		int silver = set.getInt("silver");
+    		int gold = set.getInt("gold");
+    		int total = set.getInt("total");
+    		countryList.add(new CountryRanking(countryName, createFlagImageView(imagePath), bronze, silver, gold, total));
+    	}
+    }
+    
+    public void filter(ActionEvent ev) throws SQLException {
+    	System.out.println("FILTRANDO");
+    	countryList.clear();
+    	loadList(ConnectDB.getCountryRanking());
+    	
+    }
+}
---
+++ frontend/src/application/Model/Trainer.java
@@ -0,0 +1,60 @@
+package application.Model;
+
+public class Trainer {
+    private String name;
+    private String surname;
+    private int age;
+    private String olympic;
+    private String representing;
+    
+    // Constructor
+    public Trainer(String name, String surname, int age, String olympic, String representing) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.olympic = olympic;
+        this.representing = representing;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOlympic() {
+        return olympic;
+    }
+
+    public String getRepresenting() {
+        return representing;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setOlympic(String olympic) {
+        this.olympic = olympic;
+    }
+
+    public void setRepresenting(String representing) {
+        this.representing = representing;
+    }
+}
