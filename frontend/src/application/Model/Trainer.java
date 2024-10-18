ec616a52244c63538120a1f24ae294d3b139d1a7
liangbinjie <mailandresliang@gmail.com>
2024-10-18 00:13:35 -0600

    Views Update
    Trainer add ImageView

--- frontend/src/application/Model/Trainer.java
+++ frontend/src/application/Model/Trainer.java
@@ -1,19 +1,24 @@
 package application.Model;
 
+import javafx.scene.image.ImageView;
+
 public class Trainer {
     private String name;
     private String surname;
     private int age;
     private String olympic;
     private String representing;
+//    private ImageView flag;
+    private ImageView photo;
     
     // Constructor
-    public Trainer(String name, String surname, int age, String olympic, String representing) {
+    public Trainer(String name, String surname, int age, String olympic, String representing, ImageView photo) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.olympic = olympic;
         this.representing = representing;
+        this.photo = photo;
     }
 
     // Getters
@@ -57,4 +62,12 @@ public class Trainer {
     public void setRepresenting(String representing) {
         this.representing = representing;
     }
+
+	public ImageView getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(ImageView photo) {
+		this.photo = photo;
+	}
 }
