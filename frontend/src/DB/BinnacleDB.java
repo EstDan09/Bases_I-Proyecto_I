8f2b9e03045ec75ec6a73471b5fc31e8a60161ae
justbetheleaf <128103779+justbetheleaf@users.noreply.github.com>
2024-10-19 20:11:13 -0600

    Binnacle

--- frontend/src/DB/BinnacleDB.java
+++ frontend/src/DB/BinnacleDB.java
@@ -1,5 +1,21 @@
 package DB;
 
-public class BinnacleDB {
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import application.Model.Binnacle;
+import application.Model.Province;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class BinnacleDB {
+	public static ObservableList<Binnacle> getBinnacleListByTypeAndCountry(LocalDate date) throws SQLException {
+
+		ObservableList<Binnacle> binnacle = FXCollections.observableArrayList(
+				new Binnacle("1", LocalDate.now().toString(), "09:00", "mandi", "people", "delete", "some description"));
+		return binnacle;	
+				
+	}
 }
--- frontend/src/application/Controller/adminControllers/BinnacleViewController.java
+++ frontend/src/application/Controller/adminControllers/BinnacleViewController.java
@@ -1,13 +1,20 @@
 package application.Controller.adminControllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import DB.EventsDB;
 import application.Model.Binnacle;
+import application.Model.Event;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -16,51 +23,63 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class BinnacleViewController implements Initializable {
 
     @FXML
-    private TableColumn<Binnacle, String> idColumn;
+    private TableColumn<Binnacle, String> Id;
 
     @FXML
-    private TableColumn<Binnacle, String> dateColumn;
+    private TableColumn<Binnacle, String> dateBin;
 
     @FXML
-    private TableColumn<Binnacle, String> timeColumn;
+    private TableColumn<Binnacle, String> timeBin;
 
     @FXML
-    private TableColumn<Binnacle, String> userColumn;
+    private TableColumn<Binnacle, String> UserBin;
 
     @FXML
-    private TableColumn<Binnacle, String> objectColumn;
+    private TableColumn<Binnacle, String> objectBin;
 
     @FXML
-    private TableColumn<Binnacle, String> changeTypeColumn;
+    private TableColumn<Binnacle, String> changeTypeBin;
 
     @FXML
-    private TableColumn<Binnacle, String> descriptionColumn;
+    private TableColumn<Binnacle, String> descriptionBin;
 
     @FXML
     private Label subtitle;
 
     @FXML
-    private TableView<Binnacle> table;
-
-    // Sample Data for Binnacle
-    ObservableList<Binnacle> binnacleList = FXCollections.observableArrayList(
-        new Binnacle("1", "2024-10-01", "10:00", "User1", "Object1", "Creation", "Created a new object."),
-        new Binnacle("2", "2024-10-02", "11:30", "User2", "Object2", "Update", "Updated an existing object."),
-        new Binnacle("3", "2024-10-03", "14:15", "User3", "Object3", "Deletion", "Deleted an object.")
-    );
+    private TableView<Binnacle> binTableView;
+    
+    @FXML
+    private DatePicker datefilter;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Set up the columns with the appropriate data properties
-        idColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("id"));
-        dateColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("dateBin"));
-        timeColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("timeBin"));
-        userColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("userBin"));
-        objectColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("objectBin"));
-        changeTypeColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("changeTypeBin"));
-        descriptionColumn.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("descriptionBin"));
+        Id.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("id"));
+        dateBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("dateBin"));
+        timeBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("timeBin"));
+        UserBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("userBin"));
+        objectBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("objectBin"));
+        changeTypeBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("changeTypeBin"));
+        descriptionBin.setCellValueFactory(new PropertyValueFactory<Binnacle, String>("descriptionBin"));
+        
+        datefilter.setValue(LocalDate.of(2024,1,1));
 
-        // Load the sample data into the TableView
-        table.setItems(binnacleList);
+
+        // Populate the table with data from the DB        
+		try {
+			ObservableList<Binnacle> binnacleList = DB.BinnacleDB.getBinnacleListByTypeAndCountry(LocalDate.of(2024,1,1));
+			binTableView.setItems(binnacleList);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+        
+    }
+    
+    @FXML
+    void filter(ActionEvent event) throws SQLException {
+    	ObservableList<Binnacle> binnacleList = DB.BinnacleDB.getBinnacleListByTypeAndCountry(LocalDate.of(2024,1,1));
+		binTableView.setItems(binnacleList);
     }
 }
