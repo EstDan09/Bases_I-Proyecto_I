958fd869ff841ea3823eda3cf93ba4a6beece79f
Benji <67171031+liangbinjie@users.noreply.github.com>
2024-10-20 21:10:51 -0600

    Update AdminController.java

--- frontend/src/application/Controller/AdminController.java
+++ frontend/src/application/Controller/AdminController.java
@@ -5,30 +5,54 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class AdminController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-	@FXML
-	BorderPane contentPanel;
+    @FXML
+    BorderPane contentPanel;
 
-	private Parent root;
+  
+    public void changeToPeopleScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/Admin/PeopleView.fxml"));
+        contentPanel.setCenter(root);
+    }
 
-	public void changeToPeopleScene(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("../Views/Admin/PeopleView.fxml"));
-		contentPanel.setCenter(root);
+    
+    public void changeToEventview(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/Admin/EventsView.fxml"));
+        contentPanel.setCenter(root);
+    }
 
-	}
+    public void changeToTeamView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/Admin/TeamsView.fxml"));
+        contentPanel.setCenter(root);
+    }
+    public void changeToScoreView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/Admin/ScoreView.fxml"));
+        contentPanel.setCenter(root);
+    }
+    public void changeToBinnacleView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/Admin/BinnacleView.fxml"));
+        contentPanel.setCenter(root);
+    }
 
-	public void changeToOlympicScene(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("../Views/Admin/OlympicView.fxml"));
-		contentPanel.setCenter(root);
-
-	}
-	
-	public void exitAdmin(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("../Views/LoginView.fxml"));
-		contentPanel.setCenter(root);
-	}
+    
+    
+    
+    public void exitAdmin(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Views/LoginView.fxml"));    
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root); 
+        stage.setTitle("Olympics | Register");       
+        stage.setScene(scene);
+        stage.show();
+    }
 }
